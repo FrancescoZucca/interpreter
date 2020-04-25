ec2bda6675356cb7684f08fcfbd002238b7a9728
ryzenupp@hotmail.com <ryzenupp@hotmail.com>
2020-04-24 21:29:06 -0400

    Add project files.

---
+++ LinguLexer/Lexer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace LinguLexer
+{
+    public class Lexer
+    {
+        public string[] Lex(string line)
+        {
+            string subStringArg = null;
+            int currentArrayEntry = 0;
+            if (line.Contains('\"'))
+            {
+                string[] quotes = line.Split('\"');
+                subStringArg = quotes[1];
+            }
+            if (subStringArg != null)
+            {
+                line.Replace(subStringArg, "STRING");
+            }
+            string[] tokens = line.Split(' ');
+            foreach (string i in tokens)
+            {
+                if (i == "STRING" && subStringArg != null)
+                {
+                    string value = i;
+                    value.Replace("STRING", subStringArg);
+                    tokens[currentArrayEntry] = value;
+                }
+                currentArrayEntry++;
+            }
+            return tokens;
+        }
+    }
+
+    public class Interpreter
+    {
+        public void Interpret(string[] line)
+        {
+
+        }
+    }
+}
